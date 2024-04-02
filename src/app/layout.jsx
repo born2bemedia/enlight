682ef076f1ce81d97dbf30b905fcad9462f5b173
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-02 17:33:45 +0300

    Added proloader

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Epilogue } from "next/font/google";
 import "@/public/scss/base.scss";
 import Header from "../component/Header";
 import Footer from "../component/Footer";
+import Preloader from "../component/Preloader";
 
 const epilogue = Epilogue({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={epilogue.className}>
+        <Preloader/>
         <Header />
         <main>{children}</main>
         <Footer/>
